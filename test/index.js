5d98ad021ff1c7f442ba2ef6da3169275329b577
Anna Henningsen <anna@addaleax.net>
2024-04-08 19:24:22 +0200

    Get expected version in before hook

--- test/index.js
+++ test/index.js
@@ -3,7 +3,14 @@ const assert = require('assert');
 const getGlibcVersion = require('..');
 
 describe('glibcVersion', function() {
+  let expectedGlibcVersion;
+
+  before(function() {
+    this.timeout(30_000);
+    expectedGlibcVersion = process.report.getReport().header.glibcVersionRuntime;
+  })
+
   it('returns the glibc version', function() {
-    assert.strictEqual(getGlibcVersion(), process.report.getReport().header.glibcVersionRuntime);
+    assert.strictEqual(getGlibcVersion(), expectedGlibcVersion);
   });
 });
