8d89af51acb54a51390b0e338d4619203fdc0294
Anna Henningsen <anna@addaleax.net>
2024-04-08 18:57:47 +0200

    Actually do nothing on Windows

--- binding.cc
+++ binding.cc
@@ -10,15 +10,18 @@ namespace {
 Value GetGlibcVersion(const CallbackInfo& args) {
   Env env = args.Env();
 
-  const char* (*gnu_get_libc_version)();
   const char* libc_version = nullptr;
+#ifndef _WIN32
+  const char* (*gnu_get_libc_version)();
   *(reinterpret_cast<void**>(&gnu_get_libc_version)) = dlsym(RTLD_DEFAULT, "gnu_get_libc_version");
   if (gnu_get_libc_version != nullptr) {
     libc_version = (*gnu_get_libc_version)();
   }
+#endif
   if (libc_version != nullptr) {
     return String::New(env, libc_version);
   }
+  return env.Undefined();
 }
 
 }
